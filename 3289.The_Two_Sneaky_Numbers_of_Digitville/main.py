67a4d176eb31877cd9adcb4887fa7ec86127cfdc
zemzemyayan <zemzemyayanaudi2001@gmail.com>
2025-11-01 22:39:15 +0300

    01.11.25 ilk problem kodlandı

--- 3289.The_Two_Sneaky_Numbers_of_Digitville/main.py
+++ 3289.The_Two_Sneaky_Numbers_of_Digitville/main.py
@@ -1,1 +1,9 @@
-print("hello ")
+nums=[6,5,3,5,2,1,2,7,8,9,6]
+liste=[]
+for i in range(len(nums)):  #i=0 için içerdeki dongu 10 defa donecek
+    for j in range(len(nums)):
+        if(i==j): break
+        if(nums[i]==nums[j]):
+            liste.append(nums[i])
+
+print(liste)
